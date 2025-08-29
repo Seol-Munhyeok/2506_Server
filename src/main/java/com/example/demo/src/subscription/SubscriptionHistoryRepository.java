a71ce1137f5666ab6c17e9493e103bc5ebe1cc28
Seol-Munhyeok <abc1234568@naver.com>
2025-08-29 13:16:26 +0900

    feat: 정기 결제 스케줄러 추가

--- src/main/java/com/example/demo/src/subscription/SubscriptionHistoryRepository.java
+++ src/main/java/com/example/demo/src/subscription/SubscriptionHistoryRepository.java
@@ -5,9 +5,12 @@ import com.example.demo.src.subscription.entity.SubscriptionHistory;
 import com.example.demo.src.subscription.entity.SubscriptionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface SubscriptionHistoryRepository extends JpaRepository<SubscriptionHistory, Long> {
     Optional<SubscriptionHistory> findTopBySubscriptionOrderByCreatedAtDesc(Subscription subscription);
     Optional<SubscriptionHistory> findTopBySubscriptionAndStatusOrderByCreatedAtDesc(Subscription subscription, SubscriptionStatus status);
+    List<SubscriptionHistory> findAllByStatusAndEndDateBetween(SubscriptionStatus status, LocalDate start, LocalDate end);
 }
